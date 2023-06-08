e3aa04991b3916121250e4628bf07746c7ff13c9
yugal-pande <yugal210703@gmail.com>
2023-06-09 00:29:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:emotion_detector/home.dart';
+import 'package:camera/camera.dart';
+
+List<CameraDescription>? cameras;
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
